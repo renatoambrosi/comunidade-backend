f54cdc6e4c433b7cef547bb409d113a5a038ecfd
renatoambrosi <renato.seragi@gmail.com>
2026-03-09 01:34:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const { MercadoPagoConfig, PreApproval } = require('mercadopago');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/health', (req, res) => {
